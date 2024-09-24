4ffba0442879378a515c270635ada9da7077232d
Bart Kang <bart.kang.0719@gmail.com>
2024-09-24 09:13:15 -0700

    quick demo reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState } from "react";
+import "./App.css";
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case "increment":
+      return prevState + 1;
+    case "decrement":
+      return prevState - 1;
+    case "incrementBy":
+      return prevState + action.payload;
+
+    default:
+      break;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [count2, dispatch] = useReducer(reducer, 0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+          dispatch({ type: "incrementBy", payload: 3 });
+        }}
+      >
+        count is {count} {count2}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
